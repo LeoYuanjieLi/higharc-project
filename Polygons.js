34fdb02c1923f5c0306bf1baf9818ce29870504a
Leo Li <leo.li@adidas.com>
2022-07-25 00:35:53 -0700

    find a way to unify exterior edges and construct face

--- Polygons.js
+++ Polygons.js
@@ -1,11 +1,13 @@
 class Edge {
-  constructor(v1Id, v2Id) {
+  constructor(v1Id, v2Id, v1, v2) {
     /**
      * @type {v1: Array[Number], v2: Array[Number]}
      */
     this.start = v1Id;
     this.end = v2Id;
     this.name = v1Id.toString() + "--" + v2Id.toString();
+    this.startPos = v1;
+    this.endPos = v2;
   }
 
 }
@@ -37,100 +39,169 @@ class Polygons {
   constructor(inputJson) {
     this.vertices = inputJson.vertices;
     this.rawEdges = inputJson.edges;
-    this.adjList = {};
+    this.adjList = new Map();
     this.edges = this.unweld();
-    this.exteriorEdges = {};
-    this.faces = {};
+    this.exteriorEdges = new Map();
+    this.faces = new Map();
+    this.startPtId = this.getStartPtId();
+    this.genAdjList();
   }
 
   genAdjList() {
     for (let i = 0; i < this.vertices.length; i++) {
-      this.adjList[i] = [];
+      this.adjList.set(i, []);
     }
     for (let i = 0; i < this.rawEdges.length; i++) {
       const [start, end] = this.rawEdges[i];
-      this.adjList[start].push(end);
-      this.adjList[end].push(start);
+      this.adjList.get(start).push(end);
+      this.adjList.get(end).push(start);
+    }
+  }
+
+  getNextId(curEdge, neighbors, visited, direction="left", nextAngle = 360) {
+    const curId = curEdge.end;
+    let nextId = -1;
+    for (let i = 0; i < neighbors.length; i++) {
+      const neiId = neighbors[i];
+      const e = this.edges.get(curId.toString() + "--" + neiId.toString());
+      const angle = this.edgeAngle(e, this.edges.get(`${curEdge.end}--${curEdge.start}`));
+      // trick, use the reverse of current edge so that we bypass the +- 180 degrees headache
+      if (direction === "left") {
+        if (angle < nextAngle) {
+          nextAngle = angle;
+          nextId = neiId;
+        }
+      } else if (direction === "right") {
+        if (angle > nextAngle) {
+          nextAngle = angle;
+          nextId = neiId;
+        }
+      }
+    }
+
+    return nextId;
+  }
+
+  constructFace(startEdgeName) {
+    /**
+     * BFS keep going "right"
+     * @type {any}
+     */
+    const startEdge = this.edges.get(startEdgeName);
+    const visited = new Set();
+    const allEdges = [startEdge];
+    const queue = [startEdge];
+    while (queue.length > 0) {
+      const curEdge = queue.shift();
+      const curId = curEdge.end;
+      visited.add(curEdge.name);
+      const neighbors = this.adjList.get(curId);
+
+      const nextId = this.getNextId(curEdge, neighbors, visited, "right", 0);
+      if (nextId !== -1 && !visited.has(curId.toString() + "--" + nextId.toString())) {
+        queue.push(this.edges.get(curId.toString() + "--" + nextId.toString()));
+        allEdges.push(this.edges.get(curId.toString() + "--" + nextId.toString()));
+        if (nextId === startEdge.start) {
+          break;
+        }
+      }
+    }
+
+    return allEdges.length >= 3 ? new Face(allEdges) : null;
+
+  }
+
+  genAllFaces() {
+    /**
+     * generate all faces interior and exterior, this uses a similar greedy approach
+     * (see `constructFace` method) to genExteriorEdges, but instead going "right" as much as
+     * possible so that it construct the face.
+     */
+
+    // pre-work, add all edges to the queue;
+    const queue = [];
+    for (let i = 0; i < this.vertices.length; i++) {
+      const neighbors = this.adjList.get(i);
+      const edgeNames = neighbors.map(neiId => i.toString() + "--" + neiId.toString());
+      queue.push(...edgeNames);
+    }
+    // start the search
+    while (queue.length > 0) {
+      const current = queue.shift();
+      const curFace = this.constructFace(current);  // actual work of construction
+      if (curFace != null && !this.faces.has(curFace.name)) {
+        this.faces.set(curFace.name, curFace);
+      }
     }
   }
 
   unweld() {
-    const unweldedEdges = {};
+    const unweldedEdges = new Map();
     for (let i = 0; i < this.rawEdges.length; i++) {
       const [start, end] = this.rawEdges[i];
-      const e1 = new Edge(start, end);
-      const e2 = new Edge(end, start);
-      unweldedEdges[e1.name] = e1;
-      unweldedEdges[e2.name] = e2;
+      const e1 = new Edge(start, end, this.vertices[start], this.vertices[end]);
+      const e2 = new Edge(end, start, this.vertices[end], this.vertices[start]);
+      unweldedEdges.set(e1.name, e1);
+      unweldedEdges.set(e2.name, e2);
     }
     return unweldedEdges;
   }
 
   genExteriorEdges() {
     /**
-     * BFS, greedly adding the node having the largest angle (< 180), until going back to start
+     * BFS approach, greedy adding the node having the largest angle (< 180) aka going as "left"
+     * as possible, until going back to start
+     * Note: this will not find all exterior edges however it is guarantee find the edges so that
+     * all exterior faces has AT LEAST ONE edge in this group.
      * @type {{}}
      */
-    const startId = this.getStartingPtId();
-    const queue = [startId];
-    const visited = new Set()
-    while (queue.length > 0) {
-      const curId = queue.shift();
-      visited.add(curId);
-      const neighbors = this.adjList[`${curId}`];
-
-      let nextAngle = 361;
-      let nextId = 0;
-
-      for (let i = 0; i < neighbors.length; i++) {
-        const neiId = neighbors[i];
-        if (visited.has(neiId)) {
-          continue;
-        }
-        const e = this.edges[curId.toString() + "--" + neiId.toString()];
-        if (this.edgeAngle(e) > -180 && this.edgeAngle(e) < nextAngle) {
-          nextAngle = this.edgeAngle(e);
-          nextId = neiId;
-        }
+    //find "left most" edge
+    const neighbors = this.adjList.get(this.startPtId);
+    let nextAngle = 0;
+    let nextId = -1;
+    for (let i = 0; i < neighbors.length; i++) {
+      const neiId = neighbors[i];
+      const e = this.edges.get(this.startPtId.toString() + "--" + neiId.toString());
+      const angle = this.edgeAngle(e);  // using a default edge from the function;
+      if (angle > nextAngle) {
+        nextAngle = angle;
+        nextId = neiId;
       }
-
-      if (!visited.has(nextId)) {
-        queue.push(nextId);
-      }
-      this.exteriorEdges[curId.toString() + "--" + nextId.toString()] =
-        this.edges[curId.toString() + "--" + nextId.toString()];
     }
-
+    const startEdgeName = this.startPtId.toString() + "--" + nextId.toString();
+    const exteriorEdges = this.constructFace(startEdgeName).edges;
   }
 
   totalEdgeCount() {
     return (this.rawEdges.length - this.exteriorEdges.length) * 2 + this.exteriorEdges.length;
   }
 
-  getStartingPtId() {
+  getStartPtId() {
     const yValues = this.vertices.map(item => item[1]);
     const minYValue = Math.min(...yValues);
-    const idx = yValues.indexOf(minYValue);
-    return idx;
+    return yValues.indexOf(minYValue);
   }
 
   getInteriorPointsAndEdges() {
 
   }
 
-  edgeAngle(e) {
+  edgeAngle(e, e0 = new Edge(0, 1, [0, 0], [1, 0])) {
     /**
      * calculate the given edge angle from vector [0, 1];
      * some function borrowed from:
      * https://stackoverflow.com/questions/56147279/how-to-find-angle-between-two-vectors-on-canvas
      * @type {(e1: Edge, e2: Edge) => Number}
      */
-    const firstAngle = Math.atan2(1, 0);
-    const start = this.vertices[e.start];
-    const end = this.vertices[e.end];
+    const start0 = e0.startPos;
+    const end0 = e0.endPos;
+    const firstAngle = Math.atan2(end0[1] - start0[1], end0[0] - start0[0]);
+    const start = e.startPos;
+    const end = e.endPos;
     const secondAngle = Math.atan2(end[1] - start[1], end[0] - start[0]);
-    const angle = secondAngle - firstAngle;
-    return angle * 180 / Math.PI;
+    let angle = secondAngle - firstAngle;
+    angle = angle * 180 / Math.PI;
+    return angle >= 0 ? angle : angle + 360
   }
 
 
--- Polygons.test.js
+++ Polygons.test.js
@@ -1,7 +1,7 @@
 const {Geometry, Edge, Face} = require('./Polygons');
 
 
-describe('Geometry methods', () => {
+describe('Basic Geometry methods', () => {
   const inputJson = {
     "vertices":
       [[0, 0], [200, 0], [200, 200], [0, 200]],
@@ -13,43 +13,73 @@ describe('Geometry methods', () => {
   })
 
   it('should get the point Id with minimal y value', () => {
-    expect(testGeo.getStartingPtId()).toBe(0);
+    expect(testGeo.startPtId).toBe(0);
   });
 
   it('should generate adjList', () => {
     testGeo.genAdjList()
-    expect(testGeo.adjList)
+    expect(Object.fromEntries(testGeo.adjList))
       .toEqual(
-        { '0': [ 1, 2, 3 ], '1': [ 0, 2 ], '2': [ 1, 0, 3 ], '3': [ 0, 2 ] });
-
+          { 0: [ 1, 2, 3 ],
+          1: [ 0, 2 ],
+          2: [ 1, 0, 3 ],
+          3: [ 0, 2 ] });
   });
 
   it('should calculate 90 degrees as angle', () => {
-    const e1 = testGeo.edges['0--1'];
-    const e2 = testGeo.edges['1--2'];
-    const e3 = testGeo.edges['0--2'];
-    expect(testGeo.edgeAngle(e1)).toBe(-90);
-    expect(testGeo.edgeAngle(e2)).toBe(0);
-    expect(testGeo.edgeAngle(e3)).toBe(-45);
+    const e1 = testGeo.edges.get('0--1');
+    const e2 = testGeo.edges.get('1--2');
+    const e3 = testGeo.edges.get('0--2');
+    expect(testGeo.edgeAngle(e1)).toBe(0);
+    expect(testGeo.edgeAngle(e2)).toBe(90);
+    expect(testGeo.edgeAngle(e3)).toBe(45);
   });
 
-  it('should get 4 exterior edges', () => {
+  it('should get 3 exterior edges', () => {
     testGeo.genExteriorEdges();
-    console.log("exterior edges:");
     console.log(testGeo.exteriorEdges);
   });
-});
 
+  it('should construct a face', () => {
+    const Face1 = testGeo.constructFace('0--1');
+    console.log(Face1.name);
+    const Face2 = testGeo.constructFace('0--2');
+    console.log(Face2.name);
+    const Face3 = testGeo.constructFace('0--3');
+    console.log(Face3.name);
+  });
 
-
-
-describe('test Face constructor', () => {
-  const e1 = new Edge([0, 0], [200, 0]);
-  const e2 = new Edge([200, 0], [200, 200]);
-  const e3 = new Edge([200, 200], [0, 0]);
-  it('testFace and testFace2 should return same name', () => {
-    const testFace = new Face([e1, e2, e3]);
-    const testFace2 = new Face([e1, e3, e2]);
-    expect (testFace.name).toEqual(testFace2.name);
+  it('should gen 2 faces', () => {
+    testGeo.genAllFaces();
+    // console.log(testGeo.faces);
   });
 });
+
+// describe('Advanced Geometry methods', () => {
+//   const inputJson = {
+//     "vertices":
+//       [[0, 0], [0, 1000], [200, 200], [800, 0], [800, 600]],
+//     "edges": [[0, 1], [0, 2], [2, 3], [1, 2], [2, 4], [3, 4], [1, 4]]
+//   }
+//   let testGeo;
+//   it("should construct a geometry", () => {
+//     testGeo = new Geometry(inputJson);
+//   })
+//   it('should get 3 exterior edges', () => {
+//     testGeo.genExteriorEdges();
+//     console.log(testGeo.exteriorEdges);
+//   });
+//
+// });
+//
+//
+// describe('test Face constructor', () => {
+//   const e1 = new Edge([0, 0], [200, 0]);
+//   const e2 = new Edge([200, 0], [200, 200]);
+//   const e3 = new Edge([200, 200], [0, 0]);
+//   it('testFace and testFace2 should return same name', () => {
+//     const testFace = new Face([e1, e2, e3]);
+//     const testFace2 = new Face([e1, e3, e2]);
+//     expect (testFace.name).toEqual(testFace2.name);
+//   });
+// });
