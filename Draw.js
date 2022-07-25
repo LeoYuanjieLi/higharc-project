9e3f189f691f01c5537e21674bf0079762d48303
Leo Li <leo.li@adidas.com>
2022-07-25 15:09:37 -0700

    working on displaying

--- Draw.js
+++ Draw.js
@@ -1,15 +1,35 @@
 class Draw {
   static drawPolygon(polygons) {
     const canvas = document.getElementById('canvas');
+    // if (canvas.getContext) {
+    //   const ctx = canvas.getContext('2d');
+    //
+    //   ctx.beginPath();
+    //   const [startX, startY] = polygons[0];
+    //   ctx.moveTo(startX, startY);  // first pt
+    //   polygons.slice(1,).forEach(pt => ctx.lineTo(pt[0], pt[1]));
+    //   ctx.closePath();
+    //   ctx.stroke();
+    // }
     if (canvas.getContext) {
       const ctx = canvas.getContext('2d');
 
+      // Filled triangle
       ctx.beginPath();
-      const [startX, startY] = data[0]
-      ctx.moveTo(startX, startY);  // first pt
-      data.slice(1,).forEach(pt => ctx.lineTo(pt[0], pt[1]));
+      ctx.moveTo(25, 25);
+      ctx.lineTo(105, 25);
+      ctx.lineTo(25, 105);
+      ctx.fill();
+
+      // Stroked triangle
+      ctx.beginPath();
+      ctx.moveTo(125, 125);
+      ctx.lineTo(125, 45);
+      ctx.lineTo(45, 125);
       ctx.closePath();
       ctx.stroke();
     }
   }
 }
+
+module.exports = {Draw};
---
+++ hotreload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Opening site in Chrome."
+open index.html
+
+echo "Heating up the hot-reloading furnace..."
+wach 'clear; rld chrome'
